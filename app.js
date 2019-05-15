ad4b0efd1c5b72d754702d289c1b481732cda73f
Arina Shilyaeva <arishilyaeva@gmail.com>
2019-05-15 14:07:57 +0300

    mongodb connection added

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+mongoose.connect(
+  'mongodb+srv://arishilyaeva:' + 
+        process.env.MONGO_ATLAS_PW + 
+   '@scluster-ftsg2.mongodb.net/test?retryWrites=true',
+   { 
+     useNewUrlParser: true 
+    });
+
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Header', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
+});
 
 // Routes which should handle requests
 app.use('/products', productRoutes);
